eff717c6bc12e5b5b4d043df664d51dee7404d45
Ajay Agarwal <ajay.agr11@gmail.com>
2023-10-14 14:57:51 +0530

    updated logic to fetch product on component init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
 import ProductGrid from "./components/ProductGrid/ProductGrid";
+import { TProduct } from "./types/Products";
 
 function App() {
+  const [products, setProducts] = useState<TProduct[]>([]);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    (async () => {
+      try {
+        const productData = await (await fetch("/assets/products.json")).json();
+        setProducts(productData);
+        setError(false);
+      } catch (err) {
+        setError(true);
+        console.log(err);
+      }
+    })();
+  }, []);
   return (
     <main>
-      <ProductGrid />
+      {products.length ? <ProductGrid products={products} /> : <></>}
+      {error ? (
+        <h1>Some error occured while fetching products. Please try again!!</h1>
+      ) : (
+        <></>
+      )}
     </main>
   );
 }
--- src/components/ProductGrid/ProductGrid.tsx
+++ src/components/ProductGrid/ProductGrid.tsx
@@ -1,14 +1,18 @@
 import { useRef } from "react";
 import Product from "../Product/Product";
 import ProductGridHeader from "./ProductGridHeader/ProductGridHeader";
-import { TProductFilters } from "../../types/Products";
+import { TProduct, TProductFilters } from "../../types/Products";
 import useProductGrid from "../../hooks/useProductGrid";
 import Carousel, { CarouselRef } from "../Carousel/Carousel";
 
-import products from "../../assets/products.json";
+// import products from "../../assets/products.json";
 import "./ProductGrid.scss";
 
-function ProductGrid() {
+interface IProductGridProps {
+  products: TProduct[];
+}
+
+function ProductGrid({ products }: IProductGridProps) {
   const gridRef = useRef<HTMLDivElement>(null);
   const carouselRef = useRef<CarouselRef>(null);
   const { filteredProducts, filters, updateFilters } = useProductGrid(products);
